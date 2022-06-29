a85f16b289064d0b279fbe517965004b48a99ca6
GeonwooVincentKim <kdsnop@gmail.com>
2022-06-29 11:27:54 +0900

    [Update] - Index.html and index.js has been updated

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,23 @@
 import Layout from './Layout.vue';
 import Theme from 'awesome-vitepress-theme';
 
+import DefaultTheme from 'vitepress/theme';
+
 export default {
+    // Root component to wrap each page
     Layout,
     Theme,
+    ...DefaultTheme,
+
+    // This is a Vue 3 functional Component
     NotFound: () => 'custom 404',
 
-    ehhanceApp({ app, router, siteDate }) {},
+    /* 
+        app -> Vue 3 app instance from `createApp()`
+        router -> VitePress custom router
+        siteDate -> A `ref` of current site-level metadata
+    */
+    ehhanceApp({ app, router, siteDate }) {
+        app.compoonent('MyGlobalComponent');
+    },
 };
